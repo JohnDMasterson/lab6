0f4b493f44a5b1f88f211da55152fade0baddea9
masters6 <masters6@purdue.edu>
2015-04-27 16:31:20 -0400

    Setting priority to lower priorities doesn't seem to work, also set up Makefile for testing and whatnot

--- src/pootis.c
+++ src/pootis.c
@@ -1,8 +1,25 @@
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <stdio.h>
 
 int main() {
-  setpriority(PRIO_PROCESS, 0, -20);
-  //while(1);
-  //return 1;
+ /******************************************
+ |       This area is to set up arena conditions       |
+ |        It:                                                                                     |
+ |             - Sets process priority to 19                        |
+ ******************************************/
+ #ifdef ARENATEST
+printf("Setting up arena conditions\nChanging Priority to 19\n");
+ int prioarena = getpriority(PRIO_PROCESS, 0);
+printf("Current priority is %d\nSetting arena priority to 19\n", prioarena);
+ if (setpriority(PRIO_PROCESS, 0, 19) < 0) 
+  printf("Set Prioity failed\n");
+  //perror("SETPRIORITY"); 
+ else
+  printf("Arena priority set to 19\n");
+ printf("Arena Conditions set\n\n\n");
+#endif
+
+
+
 }
